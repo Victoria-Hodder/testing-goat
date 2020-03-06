7d08056809d9eec97cc475160d5d2d71330d5650
Victoria Hodder <vrhodder@posteo.net>
2020-03-06 12:13:59 +0100

    removed one unittest and added quick fix to home page

--- lists/tests.py
+++ lists/tests.py
@@ -22,23 +22,25 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1') # sets up the test
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/') # calls code under test
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode()) # assertions
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
-
+        
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
