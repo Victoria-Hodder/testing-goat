fa431ec4821573bddef9476dc8b00dc9d946d711
Victoria Hodder <vrhodder@posteo.net>
2020-03-07 12:24:35 +0100

    created new html file for lists, adapted views and tests accordingly

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,9 @@ def home_page(request):
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/lists/the-only-list-in-the-world/')
 
-    items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
-    return render(request, 'lists/home.html', {'items': items})
+    return render(request, 'lists/list.html', {'items': items})
