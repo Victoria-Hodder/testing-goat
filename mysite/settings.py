09d1611fa0960902f4897871c8ffbe4be2ca6750
Victoria Hodder <vrhodder@posteo.net>
2020-03-11 15:04:39 +0100

    set STATIC_ROOT in settings and disable admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
