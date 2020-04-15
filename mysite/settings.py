6cf65417f53527b1b30079fae0bbd06d9fe50f0f
Victoria Hodder <vrhodder@posteo.net>
2020-04-15 10:26:43 +0200

    hack ALLOWED_HOSTS to be *

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
