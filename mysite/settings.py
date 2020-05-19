3584efc812784a38472c6e6a44c75ab87e541eb2
vrhodder <vrhodder@posteo.net>
2020-05-19 10:07:43 +0200

    added txt listing resources used for chp 10 so far

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #do I need to add a directory above??
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
