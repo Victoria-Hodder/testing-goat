c8b84204647af22fbe09c862e37b3f1c9130adea
vrhodder <vrhodder@posteo.net>
2020-09-02 11:23:05 +0200

    reached end of chp 14; added custom save function to form

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -10,8 +11,15 @@ class ItemFormTest(TestCase):
         self.assertIn('placeholder="Enter a to-do item"', form.as_p()) # renders form as HTML
         self.assertIn('class="form-control input-lg"', form.as_p())
 
-
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
--- lists/views.py
+++ lists/views.py
@@ -12,11 +12,10 @@ def new_list(request):
     form = ItemForm(data=request.POST)    
     if form.is_valid():  
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {"form": form})
-
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -24,8 +23,6 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)    
     return render(request, 'lists/list.html', {'list': list_, 'form': form})
-
-
