175ba7b44837a1485eb90c219e7daa815303e7c0
vrhodder <vrhodder@posteo.net>
2021-05-28 15:04:04 +0200

    spiked in custom passwordless auth backend

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
+]
+
+AUTH_USER_MODEL = 'accounts.ListUser'
+AUTHENTICATION_BACKENDS = [
+    'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,28 @@ USE_TZ = True
 
 STATIC_URL = '/static/' #do I need to add a directory above??
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Email authentication settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'victorias.lists.are.great@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
