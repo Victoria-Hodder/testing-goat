763c977e154aa95150a9bd21d6b1c31735bc5b62
vrhodder <vrhodder@posteo.net>
2021-06-04 15:45:06 +0200

    started mocking, created fake_send_mail function in test_views

--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class User(models.Model):
     is_anonymous = False
     is_authenticated = True
 
+# A token model to link emails with a unique ID
 class Token(models.Model):
     email = models.EmailField()
     uid = models.CharField(default=uuid.uuid4, max_length=40)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]        
+    )
+    return redirect('/')
