b00bcec068e77f08204ee379aa227e29b63bce55
vrhodder <vrhodder@posteo.net>
2021-06-09 15:02:26 +0200

    despiked custom authentication backend

---
+++ accounts/authentication.py
@@ -0,0 +1,19 @@
+from accounts.models import User, Token
+
+class PasswordlessAuthenticationBackend(object):
+
+    def authenticate(self, uid):
+        try:
+            token = Token.objects.get(uid=uid)
+            return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
+        except Token.DoesNotExist:
+            return None
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
+            
