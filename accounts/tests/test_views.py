aecadd009d7bf3947c52030161204e882dd4623b
vrhodder <vrhodder@posteo.net>
2021-06-09 15:35:01 +0200

    reduced duplication in unittests

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, call
 import accounts.views
 from accounts.models import Token
-
 
 
 class SendLoginEmailViewTest(TestCase):
@@ -56,9 +55,28 @@ class SendLoginEmailViewTest(TestCase):
         (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
         self.assertIn(expected_url, body)
 
-
+@patch('accounts.views.auth')
 class LoginViewTest(TestCase):
 
-    def test_redirects_to_home_page(self):
+    def test_redirects_to_home_page(self, mock_auth):
         response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
+
+    def test_calls_authenticate_with_uid_from_get_request(self, mock_auth):  
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.authenticate.call_args,  
+            call(uid='abcd123')  
+        )
+
+    def test_calls_auth_login_with_user_if_there_is_one(self, mock_auth):
+        response = self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(
+            mock_auth.login.call_args,
+            call(response.wsgi_request, mock_auth.authenticate.return_value)
+        )
+
+    def test_does_not_login_if_user_is_not_authenticated(self, mock_auth):
+        mock_auth.authenticate.return_value = None
+        self.client.get('/accounts/login?token=abcd123')
+        self.assertEqual(mock_auth.login.called, False)
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect
+
 
 from accounts.models import Token
 
@@ -26,4 +27,7 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
+    user = auth.authenticate(uid=request.GET.get('token'))
+    if user:
+        auth.login(request, user)
     return redirect('/')
