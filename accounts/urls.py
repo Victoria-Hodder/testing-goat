96dd60586c6ef4993661aba9adc8e2867b953294
vrhodder <vrhodder@posteo.net>
2021-06-19 10:22:28 +0200

    add logout route

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 
-from django.conf.urls import url
 from accounts import views
+from django.contrib.auth.views import logout
+from django.conf.urls import url
+
 
 urlpatterns = [
     url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
     url(r'^login$', views.login, name='login'),
+    url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
 ]
