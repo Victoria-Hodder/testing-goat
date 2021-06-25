79c2addf612603e6c24a84003e816d6c535c2bcd
vrhodder <vrhodder@posteo.net>
2021-06-25 13:47:56 +0200

    test_my_lists: precreate sessions, move login checks into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -17,7 +17,6 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self):  
         self.browser.quit()
 
-
     def wait_for(self, fn):  
         start_time = time.time()
         while True:
@@ -27,7 +26,6 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-
 
     def wait_for_row_in_list_table(self, row_text):
         start_time = time.time()
@@ -42,6 +40,19 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Log out')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for(
+            lambda: self.browser.find_element_by_name('email')
+        )
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
---
+++ functional_tests/test_my_lists.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
+from django.contrib.sessions.backends.db import SessionStore
+from .base import FunctionalTest
+User = get_user_model()
+
+
+class MyListsTest(FunctionalTest):
+
+    def create_pre_authenticated_session(self, email):
+        user = User.objects.create(email=email)
+        session = SessionStore()
+        session[SESSION_KEY] = user.pk
+        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
+        session.save()
+        ## to set a cookie we need to first visit the domain.
+        ## 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_url/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session.session_key,
+            path='/',
+        ))
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        email = 'edith@example.com'
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_out(email)
+
+        # Edith is a logged-in user
+        self.create_pre_authenticated_session(email)
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in(email)
