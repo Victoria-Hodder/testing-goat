e527cb452b6870ba3272b5f9cf972365ea20bdfb
vrhodder <vrhodder@posteo.net>
2021-07-09 10:04:15 +0200

    adding forgotten base.py file

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 import time
+from .server_tools import reset_databas
 
 MAX_WAIT = 10
 
@@ -26,6 +27,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):  
         self.browser.quit()
